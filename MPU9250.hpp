fcd985843fe0d4528a98674a99562c8feb5114ce
PyJSBiba <72986030+PyJSBiba@users.noreply.github.com>
2022-01-28 17:22:34 +0000

    Update MPU9250.hpp

--- MPU9250.hpp
+++ MPU9250.hpp
@@ -1,109 +1,39 @@
-#include <Wire.h>
-#include <TimerOne.h>
-#include "vec3.hpp"
-
-enum GYRO_FULL_SCALE : uint8_t{
-    DPS_250 = 0x00,
-    DPS_500 = 0x08,
-    DPS_1000 = 0x10,
-    DPS_2000 = 0x18
-};
-
-enum ACC_FULL : uint8_t{
-    SCALE_2_G = 0x00,
-    SCALE_4_G = 0x08,
-    SCALE_8_G = 0x10,
-    SCALE_16_G = 0x18
-};
-
-void I2Cread(const uint8_t& Address, const uint8_t& Register, const uint8_t& Nbytes, uint8_t* Data){
-    Wire.beginTransmission(Address);
-    Wire.write(Register);
-    Wire.endTransmission();
-
-    Wire.requestFrom(Address,Nbytes);
-    uint8_t index = 0;
-    while (Wire.available())
-        Data[index++]=Wire.read();
-}
-
-void I2CwriteByte(const uint8_t& Address, const uint8_t& Register, const uint8_t& Data){
-    Wire.beginTransmission(Address);
-    Wire.write(Register);
-    Wire.write(Data);
-    Wire.endTransmission();
-}
-
-vec3 MPU9250valuesToAngles(vec3 var){
-    return var*(90.f/8192.f);
-}
-
-double Radians(const double& var){
-    return var*(PI/180);
-}
-double Angle(const double& var){
-    return var*(180/PI);
-}
-
-vec3 Radians(const vec3& vec){
-    return {Radians(vec.x),Radians(vec.y),Radians(vec.z)};
-}
-vec3 Angle(const vec3& vec){
-    return {Angle(vec.x),Angle(vec.y),Angle(vec.z)};
-}
-
-
-
-class MPU9250{
-    static constexpr const size_t&& addr = 0x68;
-    static constexpr const size_t&& mag_addr = 0x0C;
-
-
-public:
-    void begin(){
-        I2CwriteByte(addr,29,0x06);
-        I2CwriteByte(addr,26,0x06);
-        I2CwriteByte(addr,27,GYRO_FULL_SCALE::DPS_1000);
-        I2CwriteByte(addr,28,ACC_FULL::SCALE_4_G);
-        I2CwriteByte(addr,0x37,0x02);
-        I2CwriteByte(mag_addr,0x0A,0x02);
-    }
-    vec3 GetAccelerometerValues(){
-        uint8_t Buf[14];
-        I2Cread(addr,0x3B,14,Buf);
-        return vec3((float)(-(Buf[0]<<8 | Buf[1])),(float)(-(Buf[2]<<8 | Buf[3])),(float)((Buf[4]<<8|Buf[5])));
-    }
-    
-
-    vec3 GetDeltaAccelerometer(){
-        static vec3 crntAccelerometer{}, accelerometer{};
-        crntAccelerometer = GetAccelerometerValues();
-        vec3 delta = crntAccelerometer - accelerometer;
-        accelerometer = crntAccelerometer;
-        return delta;
-    }
-    vec3 GetGyroscopeValues(){
-        uint8_t Buf[14];
-        I2Cread(addr,0x3B,14,Buf);
-        return vec3((float)(-(Buf[8]<<8 | Buf[9])),(float)(-(Buf[10]<<8 | Buf[11])),(float)((Buf[12]<<8 | Buf[13])));
-    }
-    vec3 GetDeltaGyroscope(){
-        static vec3 crnt{},gyro{};
-        crnt = GetGyroscopeValues();
-        vec3 delta = crnt - gyro;
-        gyro = crnt;
-        return delta;
-    }
-    vec3 GetMagnetometer(){
-        uint8_t Mag[7];
-        I2Cread(mag_addr,0x03,7,Mag);
-        return vec3((float)(-(Mag[3]<<8 | Mag[2])),(float)(-(Mag[1]<<8 | Mag[0])),(float)(-(Mag[5]<<8 | Mag[4])));
-    }
-    vec3 GetDeltaMagnetometer(){
-        static vec3 crnt{}, mag{};
-        crnt = GetMagnetometer();
-        vec3 delta = crnt - mag;
-        mag = crnt;
-        return delta;
-    }
-};
+rule_t block_r = ch_p('{') >> *(~ch_p('}') | (~ch_p('{') >> block_r));
+	rule_t bin_r = str_p("0b") >> bin_p[assign(m)];
+	rule_t hex_r = str_p("0x") >> hex_p[assign(m)];
+	rule_t dec_r = uint_p[assign(m)];
+  rule_t id_r = alpha_p >> *(alnum_p | ch_p('_') | ch_p('.'));
+  rule_t string_r = ch_p('"') >> (*(~ch_p('"')))[assign(s)] >> ch_p('"');
+  rule_t channel_r = str_p("channel") >> *(space_p) >> id_r[assign(s)][assign_a(ch.name,s)][push_back_a(lstJSVars_,s)] >> *(space_p) >> ch_p('{') >>
+    *(space_p 
+			| (str_p("mode") >> *(space_p) >> ch_p('=') >> *(space_p) >> 
+        	(str_p("in")[assign_a(ch.mode,(int)channel::IN)] | str_p("out")[assign_a(ch.mode,(int)channel::OUT)]) >> *(space_p) >> ch_p(';'))
+			| (str_p("number") >> *(space_p) >> ch_p('=') >> *(space_p) >> int_p[assign(ch.number)] >> *(space_p) >> ch_p(';')) 
+			| (str_p("channel") >> *(space_p) >> ch_p('=') >> *(space_p) >> int_p[assign(ch.ch)] >> *(space_p) >> ch_p(';')) 
+			| (str_p("gain") >> *(space_p) >> ch_p('=') >> *(space_p) >> int_p[assign(ch.gain)] >> *(space_p) >> ch_p(';')) 
+			| (str_p("type") >> *(space_p) >> ch_p('=') >> *(space_p) >>
+					(str_p("analog")[assign_a(ch.type,(int)channel::ANALOG)] | str_p("discrete")[assign_a(ch.type,(int)channel::DISCRETE)]) >> *(space_p) >> ch_p(';'))
+			| (str_p("mask") >> *(space_p) >> ch_p('=') >> *(space_p) >> 
+					(bin_r[assign_a(ch.mask,m)] | hex_r[assign_a(ch.mask,m)] | dec_r[assign_a(ch.mask,m)]) >> *(space_p) >> ch_p(';'))
+    ) >>
+    ch_p('}') >> *(space_p) >> ch_p(';'); 
+  rule_t device_r = str_p("device_info") >> *(space_p) >> id_r[assign(dev_info_.name)] >> *(space_p) >> ch_p('{') >>
+    *(space_p | 
+      (str_p("device") >> *(space_p) >> ch_p('=') >> *(space_p) >> string_r[assign_a(dev_info_.dev,s)] >> *(space_p) >> ch_p(';')) | 
+      (str_p("blocking") >> *(space_p) >> ch_p('=') >> *(space_p) >> 
+        (str_p("true")[assign_a(dev_info_.blocking,true)] | str_p("false")[assign_a(dev_info_.blocking,false)]) >> *(space_p) >> ch_p(';')) |
+      (str_p("freq_ch") >> *(space_p) >> ch_p('=') >> *(space_p) >> int_p[assign(dev_info_.freq_ch)] >> *(space_p) >> ch_p(';')) | 
+      (str_p("freq_sel_ch") >> *(space_p) >> ch_p('=') >> *(space_p) >> int_p[assign(dev_info_.freq_sel_ch)] >> *(space_p) >> ch_p(';')) |
+      (str_p("speed") >> *(space_p) >> ch_p('=') >> *(space_p) >> int_p[assign(dev_info_.speed)] >> *(space_p) >> ch_p(';')) | 
+      channel_r[assign_a(v,ch)][push_back_a(lstChannels_,v)]
+    ) >>
+    ch_p('}') >> *(space_p) >> ch_p(';');
+  rule_t var_r = (str_p("var") >> *(space_p) >> id_r[assign(s)][push_back_a(lstJSVars_,s)] >>*(~ch_p(';')) >> ch_p(';'))[assign(s)][push_back_a(lstVars,s)];
+  rule_t function_r = str_p("function") >> *(~ch_p('`'));
+	//rule_t function_r = str_p("function") >> *(~ch_p('{')) >> block_r;
+  rule_t script_r = str_p("script") >> *(space_p) >> ch_p('{') >>
+    *(space_p |
+      var_r[assign(s)][push_back_a(lstJSVars_,s)] |
+      (function_r[assign(s)][push_back_a(lstFunc,s)] >> *(space_p) >> ch_p('`'))
+    ) >>
+    ch_p('}') >> *(space_p) >> ch_p(';');
